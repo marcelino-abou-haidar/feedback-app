556bb0262e3b3a8da4e6f269fb7b9c990dc2417d
Marcelino Abou-Haidar <marcelinofabouhaidar@gmail.com>
2022-01-02 20:16:57 +0200

    feat: add feedback to array and re-render component, install uuid (Add Feedback)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import { useState } from 'react'
 import FeedbackData from './data/feedbackData'
@@ -7,16 +8,20 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 
 const App = () => {
+	const [feedback, setFeedback] = useState(FeedbackData)
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
 	const deleteFeedback = (id) => {
 		if (window.confirm(`Are you sure you want to delete feedback #${id}?`))
 			setFeedback(feedback.filter((item) => id !== item.id))
 	}
-	const [feedback, setFeedback] = useState(FeedbackData)
 	return (
 		<div>
 			<Header />
 			<div className='container'>
-				<FeedbackForm />
+				<FeedbackForm handleAdd={addFeedback} />
 				<FeedbackStats feedback={feedback} />
 				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,7 +2,7 @@ import Card from './shared/Card'
 import { useState } from 'react'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
 	const [text, setText] = useState('')
 	const [btnDisabled, setBtnDisabled] = useState(true)
 	const [message, setMessage] = useState('')
@@ -11,7 +11,7 @@ const FeedbackForm = () => {
 		if (text === '') {
 			setBtnDisabled(true)
 			setMessage(null)
-		} else if (text !== '' && text.trim().length <= 10) {
+		} else if (text !== '' && text.trim().length <= 9) {
 			setBtnDisabled(true)
 			setMessage('Text must be at least 10 characters')
 		} else {
@@ -20,9 +20,21 @@ const FeedbackForm = () => {
 		}
 		setText(e.target.value)
 	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if (text.trim().length > 10) {
+			const newFeedback = {
+				text,
+				rating,
+			}
+			handleAdd(newFeedback)
+			setText('')
+		}
+	}
 	return (
 		<Card>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h2>How would you rate your service with us?</h2>
 				<RatingSelect select={(rating) => setRating(rating)} />
 				<div className='input-group'>
