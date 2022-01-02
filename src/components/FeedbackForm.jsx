57306c69d152cac847bfd348172cec9a3b26abb5
Marcelino Abou-Haidar <marcelinofabouhaidar@gmail.com>
2022-01-02 18:49:17 +0200

    feat: add input validation (Realtime Validation)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,19 @@ import { useState } from 'react'
 import Button from './shared/Button'
 const FeedbackForm = () => {
 	const [text, setText] = useState('')
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
 	const handleTextChange = (e) => {
+		if (text === '') {
+			setBtnDisabled(true)
+			setMessage(null)
+		} else if (text !== '' && text.trim().length <= 10) {
+			setBtnDisabled(true)
+			setMessage('Text must be at least 10 characters')
+		} else {
+			setBtnDisabled(false)
+			setMessage(null)
+		}
 		setText(e.target.value)
 	}
 	return (
@@ -18,8 +30,11 @@ const FeedbackForm = () => {
 						onChange={handleTextChange}
 						value={text}
 					/>
-					<Button type='submit'>Send</Button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	)
