1d6b561263ab644eb70e1451ccaacc1962cee6f2
Marcelino Abou-Haidar <marcelinofabouhaidar@gmail.com>
2022-01-05 16:44:55 +0200

    feat: add feedback to json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
@@ -11,9 +10,7 @@ export const FeedbackProvider = ({ children }) => {
 	}, [])
 
 	const fetchFeedback = async () => {
-		const response = await fetch(
-			'http://localhost:5000/feedback?_sort=id&_order=desc'
-		)
+		const response = await fetch('/feedback?_sort=id&_order=desc')
 		const data = await response.json()
 
 		setFeedback(data)
@@ -40,9 +37,17 @@ export const FeedbackProvider = ({ children }) => {
 			setFeedback(feedback.filter((item) => id !== item.id))
 	}
 	// Add feedback
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4()
-		setFeedback([newFeedback, ...feedback])
+	const addFeedback = async (newFeedback) => {
+		const response = await fetch('/feedback', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newFeedback),
+		})
+
+		const data = await response.json()
+		setFeedback([data, ...feedback])
 	}
 	// Edit feedback item
 	const editFeedback = (item) => {
